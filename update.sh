1fb150cc00a923258293b56e277fb44335ea8e8c
Puja Ramanathan <puja.ramanathan@gmail.com>
2019-03-10 19:11:19 -0700

    Added update repo script

---
+++ update.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/DavidThien/IDE-IT.git
+cp IDE-IT/backend_plugin/src/main/evaluators/*.java src/main/evaluators/
+cp IDE-IT/backend_plugin/src/main/activation/*.java src/main/activation/
+cp IDE-IT/backend_plugin/src/main/interfaces/*.java src/main/interfaces/
+cp IDE-IT/backend_plugin/src/main/listeners/*.java src/main/listeners/
+cp IDE-IT/backend_plugin/src/test/java/*.java src/test/java
+rm -R -f IDE-IT/
+git clone https://github.com/AlyssaRicketts/IDE-IT-Frontend.git
+cp IDE-IT-Frontend/IDE-IT-Frontend/src/main/java/*.java src/main/java/
+cp IDE-IT-Frontend/IDE-IT-Frontend/src/plugin/views/*.java src/plugin/views/
+cp IDE-IT-Frontend/IDE-IT-Frontend/src/test/java/*.java src/test/java
+rm -R -f IDE-IT-Frontend/
