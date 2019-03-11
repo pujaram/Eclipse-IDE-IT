3ca7a80301009be7ad6200854b0e40b19d0b81a2
pujaram <puja.ramanathan@gmail.com>
2019-03-11 14:18:39 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ cp IDE-IT/backend_plugin/src/main/evaluators/*.java src/main/evaluators/
 cp IDE-IT/backend_plugin/src/main/activation/*.java src/main/activation/
 cp IDE-IT/backend_plugin/src/main/interfaces/*.java src/main/interfaces/
 cp IDE-IT/backend_plugin/src/main/listeners/*.java src/main/listeners/
+cp IDE-IT/backend_plugin/src/main/ASTVisitors/*.java src/main/ASTVisitors/
 cp IDE-IT/backend_plugin/src/test/java/*.java src/test/java
 rm -R -f IDE-IT/
 git clone https://github.com/AlyssaRicketts/IDE-IT-Frontend.git
